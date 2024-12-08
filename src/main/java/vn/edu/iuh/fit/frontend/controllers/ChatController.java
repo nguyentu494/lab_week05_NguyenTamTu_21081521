3e0e5470fcdccba52d73dba019852593db8d49a2
nguyentu494 <nguyentu2003.khtgdd@gmail.com>
2024-12-08 09:33:04 +0700

    add para for application

--- src/main/java/vn/edu/iuh/fit/frontend/controllers/ChatController.java
+++ src/main/java/vn/edu/iuh/fit/frontend/controllers/ChatController.java
@@ -18,6 +18,7 @@ import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.mistralai.MistralAiChatModel;
 
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,10 +32,20 @@ public class ChatController {
 
     private final MistralAiChatModel chatModel;
 
+//    @Autowired
+//    private VectorStore vectorStore;
+
     @Autowired
     public ChatController(MistralAiChatModel chatModel) {
         this.chatModel = chatModel;
     }
+
+//    @GetMapping("/ai/vector")
+//    public Map<String,String> vector(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+//
+//        return Map.of("vector", this.chatModel.vector(message).toString());
+//    }
+
 
     @GetMapping("/ai/generate")
     public Map<String,String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
